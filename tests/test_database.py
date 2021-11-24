762255ea74433a0841b8068ea496a727e286471f
Alexander <retributioniswar@gmail.com>
2021-11-24 19:44:30 +0700

    use async methods in database manager

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,16 +3,18 @@ import pytest
 from ttmm.database import SQLiteManager
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session", autouse=True)
 def database_manager():
-    return SQLiteManager('test.db')
+    manager = SQLiteManager('test.db')
+    yield manager
 
 
-def test_creating_project(database_manager):
+@pytest.mark.asyncio
+async def test_creating_project(database_manager):
     project_name = 'project_name'
     tags = ['tag1', 'tag2', 'tag3']
 
-    database_manager.add_project(project_name, tags)
-    signature = database_manager.get_project_signature(project_name)
+    await database_manager.add_project(project_name, tags)
+    signature = await database_manager.get_project_signature(project_name)
 
     assert signature == ['id'] + tags
--- ttmm/database/abstract.py
+++ ttmm/database/abstract.py
@@ -6,19 +6,19 @@ def __raise_abstract_error__(msg: Optional[str] = None):
 
 
 class AbstractDatabase:
-    def get_project_signature(self, project_name: str):
+    async def get_project_signature(self, project_name: str):
         __raise_abstract_error__()
 
-    def get_project_models_list(self, project_name: str):
+    async def get_project_models_list(self, project_name: str):
         __raise_abstract_error__()
 
-    def add_project(self, project_name: str, tags: List[str]):
+    async def add_project(self, project_name: str, tags: List[str]):
         __raise_abstract_error__()
 
-    def delete_project(self, project_name: str):
+    async def delete_project(self, project_name: str):
         __raise_abstract_error__()
 
-    def update_project(
+    async def update_project(
         self,
         project_name: str,
         names_to_add: List[str],
@@ -27,7 +27,7 @@ class AbstractDatabase:
     ):
         __raise_abstract_error__()
 
-    def add_model(
+    async def add_model(
         self,
         project_name: str,
         model_name: str,
@@ -36,10 +36,10 @@ class AbstractDatabase:
     ):
         __raise_abstract_error__()
 
-    def delete_model(self, project_name: str, model_name: str):
+    async def delete_model(self, project_name: str, model_name: str):
         __raise_abstract_error__()
 
-    def update_model(
+    async def update_model(
         self,
         project_name: str,
         model_name: str,
