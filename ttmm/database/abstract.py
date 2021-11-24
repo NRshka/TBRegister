35e2bb39be439d03d6264a9f3a410663164d8805
Alexander <retributioniswar@gmail.com>
2021-11-24 19:10:49 +0700

    move util method for raising error out of class def

--- ttmm/database/abstract.py
+++ ttmm/database/abstract.py
@@ -1,21 +1,22 @@
 from typing import Dict, List, Optional, Union
 
 
-class AbstractDatabase:
-    def __raise_abstract_error__(self, msg: Optional[str] = None):
-        raise NotImplementedError("Calling a method of AbstractDatabase!" or msg)
+def __raise_abstract_error__(msg: Optional[str] = None):
+    raise NotImplementedError("Calling a method of AbstractDatabase!" or msg)
 
+
+class AbstractDatabase:
     def get_project_signature(self, project_name: str):
-        raise self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def get_project_models_list(self, project_name: str):
-        raise self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def add_project(self, project_name: str, tags: List[str]):
-        self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def delete_project(self, project_name: str):
-        self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def update_project(
         self,
@@ -24,7 +25,7 @@ class AbstractDatabase:
         names_to_drop: List[str],
         names_to_rename: Dict[str, str]
     ):
-        self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def add_model(
         self,
@@ -33,10 +34,10 @@ class AbstractDatabase:
         model_path: str,
         tag_values: Dict[str, Union[str, int, float]]
     ):
-        self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def delete_model(self, project_name: str, model_name: str):
-        self.__raise_abstract_error__()
+        __raise_abstract_error__()
 
     def update_model(
         self,
@@ -46,4 +47,4 @@ class AbstractDatabase:
         names_to_drop: List[str],
         names_to_rename: Dict[str, str]
     ):
-        self.__raise_abstract_error__()
+        __raise_abstract_error__()
