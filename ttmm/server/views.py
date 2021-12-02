bc835c88dc026bf89367a3e80fca5ee8fec3092a
Alexander Makarov <retributioniswar@gmail.com>
2021-12-02 22:19:36 +0700

    add'list of projects' page

--- ttmm/server/views.py
+++ ttmm/server/views.py
@@ -7,3 +7,12 @@ from .server import router
 @aiohttp_jinja2.template("index.html")
 def index(request: web.Request) -> web.Response:
     return {}
+
+
+@router.get("/projects")
+@aiohttp_jinja2.template("projects.html")
+async def projects_list_page(request: web.Request):
+    db_manager = request.config_dict["DB"]
+    project_names = await db_manager.get_projects_list()
+
+    return {"projects": project_names}
